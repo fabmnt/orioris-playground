a69b545154d7c4edc98bc14376108782c563cd7b
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-11-24 14:38:08 -0600

    feat: Add a new `Playground` component and display it on the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
+import { Playground } from '@/components/playground'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <h1 className='text-5xl font-bold tracking-tight'>
-        Orioris{' '}
-        <Image
-          className='inline-block'
-          src='/oriorisai.svg'
-          alt='Orioris logo'
-          width={100}
-          height={100}
-        />
-      </h1>
+    <div className='flex min-h-screen flex-col bg-zinc-50 font-sans dark:bg-black'>
+      <header>
+        <h1 className='text-xl font-bold tracking-tight'>
+          Orioris{' '}
+          <Image
+            className='inline-block'
+            src='/oriorisai.svg'
+            alt='Orioris logo'
+            width={40}
+            height={40}
+          />
+        </h1>
+      </header>
+      <main className='mx-auto w-full max-w-4xl py-4'>
+        <Playground />
+      </main>
     </div>
   )
 }

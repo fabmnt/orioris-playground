6ad54f3fd1d5b02dda490ccd8a0f08c4952b5edc
Fabián Montoya <fabianmontoya2802@gmail.com>
2025-11-24 11:51:35 -0600

    Add configuration and type definitions for API integration

---
+++ app.config.ts
@@ -0,0 +1,21 @@
+import { Tool } from "./app.types";
+
+const ENDPOINTS = {
+  extraction: ({
+    tool,
+    processOutput,
+  }: {
+    tool: Tool;
+    processOutput: boolean;
+  }) =>
+    new URL(
+      `${config.API_URL}/${tool}/${processOutput ? "process-output" : ""}`
+    ),
+};
+
+const API_URL = "https://orioris.controlcentralcarrier.com/api/v1";
+
+export const config = {
+  API_URL,
+  ENDPOINTS,
+};
